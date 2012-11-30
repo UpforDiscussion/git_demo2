577c29443dbfdcb9e7b546046e8dbf412d52ebc8
Avinash Karnani <avi@avikarnani.com>
2012-11-30 11:04:29 -0500

    first checkin and git set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitDemo2::Application.config.secret_token = '3ef757d9285dcbf7cacd80a6ab24bb1fe057f3151fd5888951799c58960208d023fb8d072ad6f6652dd3b923a332977bb937e2eef7cc889a1587224e33e22569'
